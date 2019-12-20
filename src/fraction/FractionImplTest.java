5131b0648425596f861629a8d6a89f7fc72551c2
HPRIOR <hprior02@dcs.bbk.ac.uk>
2019-12-20 16:35:56 +0000

    added test dummy methods. can't do last method yet because it may be wrong

---
+++ src/fraction/FractionImplTest.java
@@ -0,0 +1,49 @@
+package fraction;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FractionImplTest {
+
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testSubtract() {
+    }
+
+    @Test
+    public void testMultiply() {
+    }
+
+    @Test
+    public void divide() {
+    }
+
+    @Test
+    public void abs() {
+    }
+
+    @Test
+    public void negate() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void inverse() {
+    }
+
+    @Test
+    public void compareTo() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
